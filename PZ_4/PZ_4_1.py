637767d0ac3419ceb26628c5ddf925d961c25df7
Semyon <samhaydukov@gmail.com>
2021-11-13 15:32:12 +0300

    Update

--- PZ_4/PZ_4_1.py
+++ PZ_4/PZ_4_1.py
@@ -1,3 +1,12 @@
 import math
-i=float(input('Введите цену 1 кг конфет: '))
-print(i)
+
+i = float(input('Введите цену 1 кг конфет: '))
+while type(i) != float:
+    try:
+        i = float(i)
+        if i < 0:
+            print('Вы ввели неправильное значение!')
+            i = float(input('Введите цену 1 кг конфет заново: '))
+    except ValueError:
+        print('Введено неверное значение!')
+        i = float(input('Введите цену 1 кг конфет:  '))
