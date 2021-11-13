edb34f6f194006f48c0ddfaeafed600087dd0554
Semyon <samhaydukov@gmail.com>
2021-11-13 15:47:07 +0300

    Update

--- PZ_4/PZ_4_1.py
+++ PZ_4/PZ_4_1.py
@@ -1,9 +1,9 @@
 import math
 
-kilogramik = 0.0#Присвоение переменной
+kilogramik = 0.0  # Присвоение переменной
 a = (input('Введите цену конфет: '))
 i = 0.0
-while type(a) != float:#Обработка исключений
+while type(a) != float:  # Обработка исключений
     try:
         a = float(a)
         if a < 0:
@@ -12,7 +12,7 @@ while type(a) != float:#Обработка исключений
     except ValueError:
         print('Введено неверное значение!')
         a = ('Введите вещественное число заново: ')
-while kilogramik <= 0.9:#Начало цикла
+while kilogramik <= 0.9:  # Начало цикла
     kilogramik += 0.1
-    i = a*kilogramik
-    print(round(i, 2))#Ответ
+    i = a * kilogramik
+    print(round(i, 2))  # Ответ
